3fb71d20aaaa7ebe2310b29c3334df916ebf9abd
Tzvi-H <16guitar@gmail.com>
2022-09-05 15:40:25 -0600

    3.11

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // const baseUrl = "http://localhost:3001/api/persons";
-const baseUrl = "https://sheltered-retreat-70457.herokuapp.com/api/persons";
+// const baseUrl = "https://sheltered-retreat-70457.herokuapp.com/api/persons";
+
+const baseUrl = "/api/persons";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
