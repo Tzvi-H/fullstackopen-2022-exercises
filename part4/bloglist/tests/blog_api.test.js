d6f2451ef36f1cdaccd3499b53300448f33eaf7b
Tzvi-H <16guitar@gmail.com>
2022-09-13 12:47:37 -0600

    4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+  {
+    title: "title 1",
+    author: "author 1",
+    url: "url1.com",
+    likes: 1,
+  },
+  {
+    title: "title 2",
+    author: "author 2",
+    url: "url2.com",
+    likes: 0,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("there are two blogs", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
