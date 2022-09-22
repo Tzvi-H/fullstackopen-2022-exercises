ac2864702adad23838c945befe1576d852cbb298
Tzvi-H <16guitar@gmail.com>
2022-09-22 13:30:22 -0600

    5.4

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
+
 import Blog from "./components/Blog";
 import Login from "./components/Login";
 import CreateBlog from "./components/CreateBlog";
+import Notification from "./components/Notification";
+
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -31,8 +35,15 @@ const App = () => {
       blogService.setToken(user.token);
       setUser(user);
       window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      setNotification({ type: "success", message: "successfully logged in" });
+      setTimeout(() => {
+        setNotification(null);
+      }, 3000);
     } catch (exception) {
-      alert("login failed");
+      setNotification({ type: "error", message: "wrong username or password" });
+      setTimeout(() => {
+        setNotification(null);
+      }, 3000);
     }
   };
 
@@ -40,19 +51,44 @@ const App = () => {
     setUser(null);
     window.localStorage.removeItem("loggedBlogappUser");
     blogService.setToken(null);
+    setNotification({ type: "success", message: "successfully logged out" });
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
   };
 
   const handleCreateBlog = async (blogObject) => {
-    const savedBlog = await blogService.create(blogObject);
-    setBlogs([...blogs, savedBlog]);
+    try {
+      const savedBlog = await blogService.create(blogObject);
+      setBlogs([...blogs, savedBlog]);
+      setNotification({
+        type: "success",
+        message: `a new blog "${savedBlog.title}" by ${savedBlog.author} added`,
+      });
+      setTimeout(() => {
+        setNotification(null);
+      }, 3000);
+    } catch (error) {
+      setNotification({ type: "error", message: error.response.data.error });
+      setTimeout(() => {
+        setNotification(null);
+      }, 3000);
+    }
   };
 
   if (user === null) {
-    return <Login handleLogin={handleLogin} />;
+    return (
+      <div>
+        <Notification notification={notification} />
+        <Login handleLogin={handleLogin} />
+      </div>
+    );
   }
 
   return (
     <div>
+      <Notification notification={notification} />
+
       <h2>blogs</h2>
 
       <p>
