da383ef67c39996a3b4633dcaa65d9c1b6688aaa
Tzvi-H <16guitar@gmail.com>
2022-09-23 13:35:04 -0600

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const showWhenVisible = { display: showDetails ? "" : "none" };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const buttonText = showDetails ? "hide" : "view";
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}{" "}
+      <button onClick={() => setShowDetails(!showDetails)}>{buttonText}</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.author}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
