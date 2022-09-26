c93e7f374795079ebb623dfe26d51d5e08316fb6
Tzvi-H <16guitar@gmail.com>
2022-09-26 12:06:32 -0600

    5.15

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -41,3 +41,28 @@ test("after clicking the button, children are displayed", async () => {
   const div = container.querySelector(".togglable-info");
   expect(div).not.toHaveStyle("display: none");
 });
+
+test("clicking the button twice calls event handler twice", async () => {
+  const blog = {
+    title: "test title",
+    author: "test author",
+    url: "test url",
+    likes: 3,
+    user: {},
+  };
+
+  const mockHandler = jest.fn();
+
+  render(<Blog blog={blog} handleLikeBlog={mockHandler} />);
+
+  const user = userEvent.setup();
+
+  const viewButton = screen.getByText("view");
+  await user.click(viewButton);
+
+  const likeButton = screen.getByText("like");
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  expect(mockHandler.mock.calls).toHaveLength(2);
+});
