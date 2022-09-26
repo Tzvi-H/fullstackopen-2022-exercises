a5f62090551da846d5847691e3c68fe70d93eea1
Tzvi-H <16guitar@gmail.com>
2022-09-26 12:03:37 -0600

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("renders content", () => {
@@ -12,11 +13,6 @@ test("renders content", () => {
     user: {},
   };
 
-  // render(<Blog blog={blog} />);
-
-  // const element = screen.getByText("test title");
-  // expect(element).toBeDefined();
-
   const { container } = render(<Blog blog={blog} />);
 
   const div = container.querySelector(".blog");
@@ -25,4 +21,23 @@ test("renders content", () => {
 
   const divWithUrl = container.querySelector(".togglable-info");
   expect(divWithUrl).toHaveStyle("display: none");
+});
+
+test("after clicking the button, children are displayed", async () => {
+  const blog = {
+    title: "test title",
+    author: "test author",
+    url: "test url",
+    likes: 3,
+    user: {},
+  };
+
+  const { container } = render(<Blog blog={blog} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  const div = container.querySelector(".togglable-info");
+  expect(div).not.toHaveStyle("display: none");
 });
