a94b14d74b0d681beabf2869421bc4f5908ed122
Tzvi-H <16guitar@gmail.com>
2022-10-13 11:46:01 -0600

    7.17

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 import Blogs from "./components/Blogs";
 import Blog from "./components/Blog";
@@ -45,6 +45,10 @@ const App = () => {
     }
   }, [dispatch]);
 
+  const padding = {
+    padding: 5,
+  };
+
   const handleLogin = async (username, password) => {
     const user = await loginService.login({
       username,
@@ -79,11 +83,19 @@ const App = () => {
     <div>
       <Notification />
 
-      <h2>blogs</h2>
+      <div>
+        <Link to="/" style={padding}>
+          blogs
+        </Link>
+        <Link to="/users" style={padding}>
+          users
+        </Link>
+        <span>
+          {user.name} logged in <button onClick={handleLogOut}>logout</button>
+        </span>
+      </div>
 
-      <p>
-        {user.name} logged in <button onClick={handleLogOut}>logout</button>
-      </p>
+      <h2>blogs</h2>
 
       <Routes>
         <Route
