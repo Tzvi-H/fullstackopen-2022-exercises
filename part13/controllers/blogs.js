953c354371f5c022c82f6c9e2d266ed8d21071e8
Tzvi-H <16guitar@gmail.com>
2022-11-16 12:46:14 -0600

    13.6

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -1,6 +1,11 @@
 const blogsRouter = require("express").Router();
 const { Blog } = require("../models");
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
 blogsRouter.get("/", async (req, res) => {
   const blogs = await Blog.findAll({});
   res.json(blogs);
@@ -15,8 +20,20 @@ blogsRouter.post("/", async (req, res) => {
   }
 });
 
-blogsRouter.delete("/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
+blogsRouter.put("/:id", blogFinder, async (req, res) => {
+  const blog = req.blog;
+
+  if (blog) {
+    blog.likes = req.body.likes;
+    await blog.save();
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+blogsRouter.delete("/:id", blogFinder, async (req, res) => {
+  const blog = req.blog;
 
   if (blog) {
     await blog.destroy();
